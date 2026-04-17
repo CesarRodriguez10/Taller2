4f63589a0fee99299b0f7dcc5fe9f64ebbde3ba4
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-04-17 17:16:14 -0500

    Dependencia de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.7.1")
     implementation("com.google.android.gms:play-services-tasks:18.0.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+
 }
