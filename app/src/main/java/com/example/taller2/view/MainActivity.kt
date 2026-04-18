85bd36c16234fa2708dc917fe8dbb8f7f66d6767
CesarRodriguez10 <alejorz2004@gmail.com>
2026-04-17 21:27:54 -0500

    Adelanto de Codigo MainActivity

---
+++ app/src/main/java/com/example/taller2/view/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.taller2.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.taller2.databinding.ActivityMainBinding
+import com.example.taller2.viewmodel.GameViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: GameViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.gameState.observe(this) { state ->
+            binding.txtMoney.text = "Dinero: ${state.currentMoney}"
+            binding.txtTurn.text = "Turno: ${state.currentTurn}"
+            binding.txtResult.text = state.result
+        }}}
